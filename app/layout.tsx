8910b10221307232e7ae65f0a9e44554e83dc9b9
Irvan Sandy <irvansandi3@gmail.com>
2026-02-15 13:43:52 +0700

    Fixing prod

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Mulish } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/lib/auth/auth-context";
 import { Toaster } from "sonner";
-import GlobalAuthLoader from "@/components/loaders/global-auth-loader";
+// import GlobalAuthLoader from "@/components/loaders/global-auth-loader";
 
 const mulish = Mulish({ subsets: ["latin"] });
 const title = "Pay Wise";
@@ -21,7 +21,7 @@ export default function RootLayout({
       </head>
       <body className={mulish.className}>
         <AuthProvider>
-          <GlobalAuthLoader />
+          {/* <GlobalAuthLoader /> */}
           {children}
         </AuthProvider>
         <Toaster position="top-right" richColors />
