aa441e6e251c128fad62ed634613e5134102c344
Irvan Sandy <irvansandi3@gmail.com>
2026-02-15 13:49:31 +0700

    Fixing prod

--- components/loaders/global-auth-loader.tsx
+++ components/loaders/global-auth-loader.tsx
@@ -1,30 +1,30 @@
-// // components/global-auth-loader.tsx
+// components/global-auth-loader.tsx
 
-// "use client";
+"use client";
 
-// import { Loader2 } from "lucide-react";
-// import { useAuth } from "@/lib/auth/auth-context";
-// import { motion, AnimatePresence } from "framer-motion";
+import { Loader2 } from "lucide-react";
+import { useAuth } from "@/lib/auth/auth-context";
+import { motion, AnimatePresence } from "framer-motion";
 
-// export default function GlobalAuthLoader() {
-//   const { isLoggingIn } = useAuth();
+export default function GlobalAuthLoader() {
+  const { isLoggingIn } = useAuth();
 
-//   return (
-//     <AnimatePresence>
-//       {isLoggingIn && (
-//         <motion.div
-//           className="fixed inset-0 z-9999 flex items-center justify-center bg-white/70 backdrop-blur-sm"
-//           initial={{ opacity: 0 }}
-//           animate={{ opacity: 1 }}
-//           exit={{ opacity: 0 }}
-//           transition={{ duration: 0.2 }}
-//         >
-//           <div className="flex flex-col items-center gap-3">
-//             <Loader2 className="h-10 w-10 animate-spin text-blue-600" />
-//             <p className="text-sm text-gray-600">Memuat dashboard...</p>
-//           </div>
-//         </motion.div>
-//       )}
-//     </AnimatePresence>
-//   );
-// }
+  return (
+    <AnimatePresence>
+      {isLoggingIn && (
+        <motion.div
+          className="fixed inset-0 z-9999 flex items-center justify-center bg-white/70 backdrop-blur-sm"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.2 }}
+        >
+          <div className="flex flex-col items-center gap-3">
+            <Loader2 className="h-10 w-10 animate-spin text-blue-600" />
+            <p className="text-sm text-gray-600">Memuat dashboard...</p>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+}
